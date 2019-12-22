3efa198f68139b9347936bbdd1a0ab308d989662
Steven Lariau <obs145628@gmail.com>
2019-12-22 22:06:41 +0100

    [MINI-CALC-EVAL] add tests with string stream

--- apps/mini-calc-eval/src/main.rs
+++ apps/mini-calc-eval/src/main.rs
@@ -27,43 +27,56 @@ mod tests {
         parser::Parser::new_from_file(path).eval()
     }
 
+    fn eval_file_from_str(path: &str) -> value::Value {
+        let data = std::fs::read_to_string(path).unwrap();
+        parser::Parser::new_from_str(&data).eval()
+    }
+
     #[test]
     fn test_c1() {
         assert_eq!(eval_file("./ex/c1.txt").get_int(), 34);
+        assert_eq!(eval_file_from_str("./ex/c1.txt").get_int(), 34);
     }
 
     #[test]
     fn test_c2() {
         assert_eq!(eval_file("./ex/c2.txt").get_float(), 18.5);
+        assert_eq!(eval_file_from_str("./ex/c2.txt").get_float(), 18.5);
     }
 
     #[test]
     fn test_addsub1() {
         assert_eq!(eval_file("./ex/addsub1.txt").get_int(), 15);
+        assert_eq!(eval_file_from_str("./ex/addsub1.txt").get_int(), 15);
     }
 
     #[test]
     fn test_muldiv1() {
         assert_eq!(eval_file("./ex/muldiv1.txt").get_int(), 2);
+        assert_eq!(eval_file_from_str("./ex/muldiv1.txt").get_int(), 2);
     }
 
     #[test]
     fn test_unary1() {
         assert_eq!(eval_file("./ex/unary1.txt").get_int(), -12);
+        assert_eq!(eval_file_from_str("./ex/unary1.txt").get_int(), -12);
     }
 
     #[test]
     fn test_paren1() {
         assert_eq!(eval_file("./ex/paren1.txt").get_int(), 29);
+        assert_eq!(eval_file_from_str("./ex/unary1.txt").get_int(), -12);
     }
 
     #[test]
     fn test_ops1() {
         assert_eq!(eval_file("./ex/ops1.txt").get_int(), -41);
+        assert_eq!(eval_file_from_str("./ex/ops1.txt").get_int(), -41);
     }
 
     #[test]
     fn test_ops2() {
         assert_eq!(eval_file("./ex/ops2.txt").get_float(), 3.5);
+        assert_eq!(eval_file_from_str("./ex/ops2.txt").get_float(), 3.5);
     }
 }
