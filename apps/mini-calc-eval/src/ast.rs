aab6ed5b30080ee5ec206886ef59ad0613904d1c
Steven Lariau <obs145628@gmail.com>
2019-12-25 17:16:34 +0100

    [MINI-CALC-EVAL] Use visitor design pattern to evaluate AST

--- apps/mini-calc-eval/src/ast.rs
+++ apps/mini-calc-eval/src/ast.rs
@@ -1,6 +1,6 @@
 use crate::value::Value;
 
-pub trait AST {
+pub trait AST: Visitable {
     fn dump(&self);
 }
 
@@ -13,6 +13,12 @@ pub struct ASTConst {
 impl ASTConst {
     pub fn new(val: Value) -> Box<ASTConst> {
         Box::new(ASTConst { val })
+    }
+}
+
+impl Visitable for ASTConst {
+    fn accept(&self, v: &mut dyn ASTVisitor) {
+        v.visit_const(self);
     }
 }
 
@@ -65,6 +71,12 @@ impl ASTBinOp {
     }
 }
 
+impl Visitable for ASTBinOp {
+    fn accept(&self, v: &mut dyn ASTVisitor) {
+        v.visit_binop(self);
+    }
+}
+
 impl AST for ASTBinOp {
     fn dump(&self) {
         print!("(");
@@ -86,10 +98,75 @@ impl ASTUnop {
     }
 }
 
+impl Visitable for ASTUnop {
+    fn accept(&self, v: &mut dyn ASTVisitor) {
+        v.visit_unop(self);
+    }
+}
+
 impl AST for ASTUnop {
     fn dump(&self) {
         print!("{}(", self.op);
         self.right.dump();
         print!(")");
+    }
+}
+
+pub trait ASTVisitor {
+    fn visit_const(&mut self, ast: &ASTConst);
+    fn visit_binop(&mut self, ast: &ASTBinOp);
+    fn visit_unop(&mut self, ast: &ASTUnop);
+}
+
+pub trait Visitable {
+    fn accept(&self, v: &mut dyn ASTVisitor);
+}
+
+pub struct ASTEval {
+    res: Value,
+}
+
+impl ASTEval {
+    fn get_eval(&mut self, ast: &dyn AST) -> Value {
+        ast.accept(self);
+        self.res
+    }
+
+    pub fn eval(ast: &dyn AST) -> Value {
+        (ASTEval {
+            res: Value::VInt(0),
+        })
+        .get_eval(ast)
+    }
+}
+
+impl ASTVisitor for ASTEval {
+    fn visit_const(&mut self, ast: &ASTConst) {
+        self.res = ast.val;
+    }
+
+    fn visit_binop(&mut self, ast: &ASTBinOp) {
+        match ast.op {
+            ASTBinopOp::Add => {
+                self.res = Value::add(&self.get_eval(&*ast.left), &self.get_eval(&*ast.right))
+            }
+            ASTBinopOp::Sub => {
+                self.res = Value::sub(&self.get_eval(&*ast.left), &self.get_eval(&*ast.right))
+            }
+            ASTBinopOp::Mul => {
+                self.res = Value::mul(&self.get_eval(&*ast.left), &self.get_eval(&*ast.right))
+            }
+            ASTBinopOp::Div => {
+                self.res = Value::div(&self.get_eval(&*ast.left), &self.get_eval(&*ast.right))
+            }
+        }
+    }
+
+    fn visit_unop(&mut self, ast: &ASTUnop) {
+        match ast.op {
+            '+' => self.res = self.get_eval(&*ast.right),
+            '-' => self.res = Value::sub(&Value::VInt(0), &self.get_eval(&*ast.right)),
+            _ => panic!("Cannot eval ASTUnop, invalid op '{}'", ast.op),
+        }
     }
 }
--- apps/mini-calc-eval/src/main.rs
+++ apps/mini-calc-eval/src/main.rs
@@ -11,12 +11,11 @@ fn main() {
         }
 
         Some(expr) => {
-            let ast = parser::ParserAST::new_from_str(&expr).build();
-            ast.dump();
+            let ca = parser::ParserAST::new_from_str(&expr).build();
+            ca.dump();
             println!("");
 
-            let mut rt = parser::ParserEval::new_from_str(&expr);
-            match rt.eval() {
+            match ast::ASTEval::eval(&*ca) {
                 value::Value::VInt(x) => println!("{}", x),
                 value::Value::VFloat(x) => println!("{}", x),
             }
@@ -32,6 +31,11 @@ mod tests {
         parser::ParserEval::new_from_file(path).eval()
     }
 
+    fn eval_file_ast(path: &str) -> value::Value {
+        let t = parser::ParserAST::new_from_file(&path).build();
+        ast::ASTEval::eval(&*t)
+    }
+
     fn eval_file_from_str(path: &str) -> value::Value {
         let data = std::fs::read_to_string(path).unwrap();
         parser::ParserEval::new_from_str(&data).eval()
@@ -40,48 +44,56 @@ mod tests {
     #[test]
     fn test_c1() {
         assert_eq!(eval_file("./ex/c1.txt").get_int(), 34);
+        assert_eq!(eval_file_ast("./ex/c1.txt").get_int(), 34);
         assert_eq!(eval_file_from_str("./ex/c1.txt").get_int(), 34);
     }
 
     #[test]
     fn test_c2() {
         assert_eq!(eval_file("./ex/c2.txt").get_float(), 18.5);
+        assert_eq!(eval_file_ast("./ex/c2.txt").get_float(), 18.5);
         assert_eq!(eval_file_from_str("./ex/c2.txt").get_float(), 18.5);
     }
 
     #[test]
     fn test_addsub1() {
         assert_eq!(eval_file("./ex/addsub1.txt").get_int(), 15);
+        assert_eq!(eval_file_ast("./ex/addsub1.txt").get_int(), 15);
         assert_eq!(eval_file_from_str("./ex/addsub1.txt").get_int(), 15);
     }
 
     #[test]
     fn test_muldiv1() {
         assert_eq!(eval_file("./ex/muldiv1.txt").get_int(), 2);
+        assert_eq!(eval_file_ast("./ex/muldiv1.txt").get_int(), 2);
         assert_eq!(eval_file_from_str("./ex/muldiv1.txt").get_int(), 2);
     }
 
     #[test]
     fn test_unary1() {
         assert_eq!(eval_file("./ex/unary1.txt").get_int(), -12);
+        assert_eq!(eval_file_ast("./ex/unary1.txt").get_int(), -12);
         assert_eq!(eval_file_from_str("./ex/unary1.txt").get_int(), -12);
     }
 
     #[test]
     fn test_paren1() {
         assert_eq!(eval_file("./ex/paren1.txt").get_int(), 29);
+        assert_eq!(eval_file_ast("./ex/paren1.txt").get_int(), 29);
         assert_eq!(eval_file_from_str("./ex/unary1.txt").get_int(), -12);
     }
 
     #[test]
     fn test_ops1() {
         assert_eq!(eval_file("./ex/ops1.txt").get_int(), -41);
+        assert_eq!(eval_file_ast("./ex/ops1.txt").get_int(), -41);
         assert_eq!(eval_file_from_str("./ex/ops1.txt").get_int(), -41);
     }
 
     #[test]
     fn test_ops2() {
         assert_eq!(eval_file("./ex/ops2.txt").get_float(), 3.5);
+        assert_eq!(eval_file_ast("./ex/ops2.txt").get_float(), 3.5);
         assert_eq!(eval_file_from_str("./ex/ops2.txt").get_float(), 3.5);
     }
 }
