62087356fee851fade5a240044ec71805d3d15df
Steven Lariau <obs145628@gmail.com>
2020-01-26 19:51:48 +0100

    [GENERAL] Add script file to test all cargo projects

---
+++ check.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Basic script to run cargo build and cargo test on all projects
+
+check_proj() {
+    (
+	echo "Testing $1 ..."
+	cd ./$1;
+	cargo build && cargo test
+    )
+    if [ $? -ne 0 ]; then {
+	echo "Tests failed for project $1";
+	exit 1
+    } fi
+}
+
+check_proj libs/irint3a
+check_proj libs/lanexpr
+check_proj libs/oblexer
+check_proj libs/obparser
+check_proj libs/obuid
+
+check_proj apps/cl-lanexpr
+check_proj apps/irint3a-utils/
+check_proj apps/lexer-list/
+check_proj apps/mini-calc-eval/
